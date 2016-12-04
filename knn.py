518d0c124f7ead2b709b92fd968a0021c8dbc76c
Carl Lemaire <c.lemaire@live.ca>
2016-12-03 19:16:28 -0500

    Use other datasets

--- knn.py
+++ knn.py
@@ -1,73 +1,93 @@
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 
-print("Load ratings")
+
+def load_data():
+    print("Load ratings")
+    ratings = np.zeros((N, M))
+    with open("../ratedMatrix.txt", "r") as file:
+        for line in file.readlines():
+            tokens = line.strip().split(' ')
+            u = int(tokens[0])
+            i = int(tokens[1])
+            r = float(tokens[2].replace(',', '.'))
+            ratings[u, i] = r
+
+    print("Load has_rated")
+    # Load similarity data
+    has_rated = np.zeros(ratings.shape)
+    with open("../userMovieRated.txt", "r") as file:
+        for line in file.readlines():
+            tokens = line.strip().split(' ')
+            u = int(tokens[0])
+            i = int(tokens[1])
+            did_rate = int(tokens[2])
+            has_rated[u, i] = did_rate
+
+    print("Load mean, stddev")
+    # Load test data
+    user_mean = np.zeros((N,))
+    user_stddev = np.zeros((N,))
+    with open("../moyStdev.txt", "r") as file:
+        for u, line in enumerate(file.readlines()):
+            tokens = line.strip().split(' ')
+            mean = float(tokens[0].replace(',', '.'))
+            stddev = float(tokens[1].replace(',', '.'))
+            user_mean[u] = mean
+            user_stddev[u] = stddev
+
+    return ratings, has_rated, user_mean, user_stddev
+
+
+def preprocess():
+    print("Preprocess")
+
+    # Load base data
+    data_raw = np.genfromtxt("../" + set_name + ".base", delimiter="\t", usecols=(0, 1, 2))
+
+    # Make r_ui matrix (1-5 ratings)
+    data = np.zeros((N, M))
+    for rate_pair in data_raw:
+        u = int(rate_pair[0]) - 1
+        i = int(rate_pair[1]) - 1
+        r = rate_pair[2]
+        data[u, i] = r
+
+    has_rated = (data >= 1)
+    has_not_rated = np.logical_not(has_rated)
+    nandata = data
+    nandata[has_not_rated] = np.nan
+
+    user_mean = np.nanmean(nandata, axis=1)
+    user_stddev = np.nanstd(nandata, axis=1)
+
+    # Get normalized ratings
+    r_ui = (data - user_mean[:, None]) / user_stddev[:, None]
+    r_ui[has_not_rated] = 0
+
+    return r_ui, has_rated, user_mean, user_stddev
+
+
 # Load Z normalized ratings data
 N = 943
 M = 1682
-r_ui = np.zeros((N, M))
-with open("../ratedMatrix.txt", "r") as file:
-    for line in file.readlines():
-        tokens = line.strip().split(' ')
-        u = int(tokens[0])
-        i = int(tokens[1])
-        r = float(tokens[2].replace(',', '.'))
-        r_ui[u, i] = r
-
-print("Load has_rated")
-# Load similarity data
-has_rated = np.zeros(r_ui.shape)
-with open("../userMovieRated.txt", "r") as file:
-    for line in file.readlines():
-        tokens = line.strip().split(' ')
-        u = int(tokens[0])
-        i = int(tokens[1])
-        did_rate = int(tokens[2])
-        has_rated[u, i] = did_rate
-
-print("Load mean, stddev")
-# Load test data
-user_mean = np.zeros((N,))
-user_stddev = np.zeros((N,))
-with open("../moyStdev.txt", "r") as file:
-    for u, line in enumerate(file.readlines()):
-        tokens = line.strip().split(' ')
-        mean = float(tokens[0].replace(',', '.'))
-        stddev = float(tokens[1].replace(',', '.'))
-        user_mean[u] = mean
-        user_stddev[u] = stddev
+set_name = "u1"
+r_ui, has_rated, user_mean, user_stddev = preprocess()  # preprocess load_data
 
 print("Load test data")
 # Load test data
-test_data = np.genfromtxt("../u1.test", delimiter="\t", usecols=(0, 1, 2))
-# with open("../u1.test", "r") as file:
-#     for line in file.readlines():
-#         tokens = line.strip().split('\t')
-#         u = int(tokens[0])
-#         i = int(tokens[1])
-#         rating = int(tokens[2])
-#         test_data[u, i] = rating
-#
-# np.savetxt("fml.txt", test_data[0:100])
+test_data = np.genfromtxt("../" + set_name + ".test", delimiter="\t", usecols=(0, 1, 2))
 
-# raw_data = np.genfromtxt("../u1.base", delimiter="\t", usecols=(0, 1, 2))  # 3 columns : user_id, movie_id, rating
-# rating = np.genfromtxt("../ratedMatrix.txt", delimiter=" ")
-# has_rated = np.genfromtxt("../userMovieRated.txt", delimiter=" ")
-
-# np.savetxt("out.csv", r_ui[:1000])
 print("Compute similarity")
 # Compute similarity
 distance_condensed = pdist(r_ui, metric='wminkowski', p=2, w=has_rated)
 distance = squareform(distance_condensed)
-print(distance.shape)
 w_uv = 1.0 / (0.0001 + distance)
 
 print("Adjust similarity according to commonly rated movies")
 # Adjust similarity according to commonly rated movies
 beta = 4
 I_uv = np.dot(has_rated, has_rated.transpose())
-print(I_uv.shape)
-print(w_uv.shape)
 w_prime = I_uv / (I_uv + beta) * w_uv
 
 print("Sort by similarity")
@@ -115,7 +135,11 @@ for i, user_movie_pair in enumerate(test_data):
     err += np.abs(real_rating - estimated)
     compare[i] = (real_rating, estimated)
 
-print("Erreur moyenne en etoiles:", err / len(test_data))
+print("Erreur moyenne en etoiles:", err / (len(test_data) - num_not_enough - num_not_similar))
 print("Cas pas assez de voisins:", num_not_enough)
 print("Cas pas assez similaires:", num_not_similar)
 np.savetxt("predict.csv", compare)
+
+# Erreur moyenne en etoiles: 1.04099029627
+# Cas pas assez de voisins: 54
+# Cas pas assez similaires: 6
